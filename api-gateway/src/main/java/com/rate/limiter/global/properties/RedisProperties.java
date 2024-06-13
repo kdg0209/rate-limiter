e72064f491f25754149a3911c13718d405cfedf2
torder-KDG <kdg1@torder.co.kr>
2024-06-13 11:49:29 +0900

    feat: redis 연동

---
+++ api-gateway/src/main/java/com/rate/limiter/global/properties/RedisProperties.java
@@ -0,0 +1,7 @@
+package com.rate.limiter.global.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.data.redis")
+public record RedisProperties (String host, int port){
+}
